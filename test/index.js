db5e69bdae97d9568112f6668229e86ca879c9b4
Evan Hahn <me@evanhahn.com>
2023-05-29 14:57:42 +0000

    Switch from Karma + Jasmine to Puppeteer

--- test/index.js
+++ test/index.js
@@ -1,98 +1,25 @@
-/* global untouched */
+const puppeteer = require("puppeteer");
+const path = require("path");
+const { pathToFileURL } = require("url");
 
-describe("Untouched", function () {
-  /* eslint-disable no-extend-native */
-  beforeEach(function () {
-    this.untouched = untouched();
+async function main() {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
 
-    this.originalArrayMap = Array.prototype.map;
-    this.originalJsonStringify = JSON.stringify;
+  const testPagePath = path.join(__dirname, "test.html");
+  await page.goto(pathToFileURL(testPagePath));
 
-    Array.prototype.foo = function () {
-      return "boo";
-    };
-    Array.prototype.map = function () {
-      return "wow";
-    };
+  const resultEl = await page.waitForSelector("#result", { timeout: 5000 });
+  const result = await resultEl?.evaluate((el) => el.textContent);
 
-    JSON.foo = "dope";
-    JSON.stringify = function () {
-      return "haha dope";
-    };
-  });
+  await browser.close();
 
-  afterEach(function () {
-    this.untouched.destroy();
+  if (result !== "ok") {
+    throw new Error(result);
+  }
+}
 
-    delete Array.prototype.foo;
-    Array.prototype.map = this.originalArrayMap;
-
-    delete JSON.foo;
-    JSON.stringify = this.originalJsonStringify;
-  });
-  /* eslint-enable no-extend-native */
-
-  it("returns a different Array object", function () {
-    expect(this.untouched("Array")).not.toBe(Array);
-  });
-
-  it("returns a different JSON object", function () {
-    expect(this.untouched("JSON")).not.toBe(JSON);
-  });
-
-  it("can make a new Array that works fine", function () {
-    var UntouchedArray = this.untouched("Array");
-    var arr = new UntouchedArray();
-
-    expect(arr.length).toEqual(0);
-
-    arr.push(1);
-    arr.push(2);
-
-    expect(arr.length).toEqual(2);
-    expect(arr[0]).toEqual(1);
-    expect(arr[1]).toEqual(2);
-    expect(arr.join(" and ")).toEqual("1 and 2");
-  });
-
-  it("ignores methods added to Array.prototype", function () {
-    expect([].foo()).toEqual("boo");
-
-    var UntouchedArray = this.untouched("Array");
-    expect(new UntouchedArray().foo).toBeUndefined();
-  });
-
-  it("uses un-monkeypatched Array methods", function () {
-    function square(n) {
-      return n * n;
-    }
-
-    expect([1, 2, 3].map(square)).toEqual("wow");
-
-    var UntouchedArray = this.untouched("Array");
-    var arr = new UntouchedArray(1, 2, 3);
-    expect(arr.map(square)).toEqual([1, 4, 9]);
-  });
-
-  it("can use the JSON object just fine", function () {
-    var untouchedJson = this.untouched("JSON");
-
-    expect(untouchedJson.parse("{}")).toEqual({});
-    expect(untouchedJson.parse('{"hi": 5}')).toEqual({ hi: 5 });
-
-    expect(untouchedJson.stringify({})).toEqual("{}");
-    expect(untouchedJson.stringify({ hi: 5 })).toEqual('{"hi":5}');
-  });
-
-  it("ignores properties added to the JSON object", function () {
-    expect(JSON.foo).toEqual("dope");
-
-    expect(this.untouched("JSON").foo).toBeUndefined();
-  });
-
-  it("uses un-monkeypatched JSON methods", function () {
-    expect(JSON.stringify({ hi: 5 })).toEqual("haha dope");
-
-    expect(this.untouched("JSON").stringify({ hi: 5 })).toEqual('{"hi":5}');
-  });
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
---
+++ test/test.js
@@ -0,0 +1,50 @@
+/* global untouched */
+
+function assert(condition, message) {
+  if (!condition) {
+    throw new Error(message);
+  }
+}
+
+function runTests() {
+  /* eslint-disable no-extend-native */
+  Array.prototype.foo = () => "boo";
+  Array.prototype.map = () => "wow";
+  /* eslint-enable no-extend-native */
+
+  assert([].foo() === "boo", "test extends Array to add `foo`");
+  assert([].map() === "wow", "test extends Array to change `map``");
+
+  const ut = untouched();
+
+  assert(ut("Array") !== Array, "should return a different Array object");
+  assert(ut("JSON") !== JSON, "should return a different JSON object");
+
+  const UntouchedArray = ut("Array");
+  const untouchedArray = new UntouchedArray();
+  untouchedArray.push(1, 2, 3);
+  assert(
+    untouchedArray.foo === undefined,
+    "should not have extra properties attached"
+  );
+  assert(
+    untouchedArray.map((n) => n + 1).join() === "2,3,4",
+    "should use an untouched Array map"
+  );
+
+  ut.destroy();
+}
+
+function main() {
+  const resultEl = document.createElement("div");
+  resultEl.id = "result";
+  try {
+    runTests();
+    resultEl.innerText = "ok";
+  } catch (err) {
+    resultEl.innerText = err.message;
+  }
+  document.body.append(resultEl);
+}
+
+main();
